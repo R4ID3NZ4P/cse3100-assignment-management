fbffeebc3049d6aa52a77022b115d5ea46f8c166
R4ID3NZ4P <iamxtremesifat@gmail.com>
2024-12-05 10:45:43 +0600

    Mantine ui fixed

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,46 +1,34 @@
-// import { AppShell, Burger } from "@mantine/core";
-// import { useDisclosure } from "@mantine/hooks";
-// import { Outlet } from "react-router";
-
+import { AppShell, Burger, Title } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { Outlet } from "react-router";
 
-// const Root = () => {
-//     const [opened, { toggle }] = useDisclosure();
-
-//     return (
-//         <AppShell
-//             header={{ height: 60 }}
-//             navbar={{
-//                 width: 300,
-//                 breakpoint: "sm",
-//                 collapsed: { mobile: !opened },
-//             }}
-//             padding="md"
-//         >
-//             <AppShell.Header>
-//                 <Burger
-//                     opened={opened}
-//                     onClick={toggle}
-//                     hiddenFrom="sm"
-//                     size="sm"
-//                 />
-//                 <div>Logo</div>
-//             </AppShell.Header>
-
-//             <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-
-//             <AppShell.Main><Outlet/></AppShell.Main>
-//         </AppShell>
-//     );
-// };
-
-// export default Root;
-
 const Root = () => {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
-        <div>
-            <Outlet/>
-        </div>
+        <AppShell
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+                collapsed: { mobile: !opened },
+            }}
+            padding="md"
+        >
+            <AppShell.Header>
+                <Burger
+                    opened={opened}
+                    onClick={toggle}
+                    hiddenFrom="sm"
+                    size="sm"
+                />
+                <div className="ml-2"><Title order={3}>Assignment Management</Title></div>
+            </AppShell.Header>
+
+            <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+            <AppShell.Main><Outlet/></AppShell.Main>
+        </AppShell>
     );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import '@mantine/core/styles.css';
 import { BrowserRouter } from 'react-router'
 import Router from './routes/Router.tsx'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+    <MantineProvider>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </MantineProvider>
   </StrictMode>,
 )
